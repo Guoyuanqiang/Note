11768004cd8f51d6c3635fd4e64665cd30c72782
guoyuanqiang <guoyuanqiang2008@126.com>
2016-01-18 21:49:20 +0800

    Test

---
+++ build/Note.build/Debug-iphoneos/Note.build/Objects-normal/armv7/NoteAppDelegate.d
@@ -0,0 +1,2 @@
+dependencies: /Users/guoyuanqiang/Desktop/Note/Note/NoteAppDelegate.m \
+  /Users/guoyuanqiang/Desktop/Note/Note/NoteAppDelegate.h
---
+++ build/Note.build/Debug-iphoneos/Note.build/Objects-normal/armv7/NotePageViewController.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/guoyuanqiang/Desktop/Note/Note/NotePageViewController.m \
+  /Users/guoyuanqiang/Desktop/Note/Note/NotePageViewController.h \
+  /Users/guoyuanqiang/Desktop/Note/Note/NoteContentViewController.h \
+  /Users/guoyuanqiang/Desktop/Note/Note/NoteAppDelegate.h \
+  /Users/guoyuanqiang/Desktop/Note/Note/Content.h
